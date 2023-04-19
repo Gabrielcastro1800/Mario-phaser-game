e16ceb8df058fbf0f61a53bd19a782a003946191
Gabrielcastro1800 <118899849+Gabrielcastro1800@users.noreply.github.com>
2023-04-19 15:51:54 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ var config = {
         }
     },
 };
-var plataforma
+var plataforma;
+var mariop;
+var cursors;
 
 var game = new Phaser.Game(config);
 
@@ -27,12 +29,37 @@ function preload ()
 
 function create ()
 {
+    cursors = this.input.keyboard.createCursorKeys();
+    this.physics.add.collider(mariop, plataforma);
+    mariop = this.physics.add.sprite(100, 450, "mario");
+    mariop.body.setGravityY(300)
+    mariop.setCollideWorldBounds(true);
     plataforma = this.physics.add.staticGroup();
-    plataforma.create(600, 400, "brick").setScale(0.5,0.5);
+    plataforma.create(600, 400, "brick").setScale(3.5,0.5);
     
 
 }
 
 function update ()
 {
+    if (cursors.left.isDown)
+    {
+        player.setVelocityX(-160);
+    
+    }
+    else if (cursors.right.isDown)
+    {
+        player.setVelocityX(160);
+    
+    }
+    else
+    {
+        player.setVelocityX(0);
+    
+    }
+    
+    if (cursors.up.isDown && player.body.touching.down)
+    {
+        player.setVelocityY(-330);
+    }
 }
