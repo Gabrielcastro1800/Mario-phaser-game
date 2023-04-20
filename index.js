8f5d75ac6de843ca927980a6053cb7bbbc2e1809
Gabrielcastro1800 <118899849+Gabrielcastro1800@users.noreply.github.com>
2023-04-20 10:50:18 -0300

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function preload ()
 {
         this.load.spritesheet("mario", 
         "mario.png",
-        { frameWidth: 32, frameHeight: 32 }
+        { frameWidth: 40, frameHeight: 40 }
     );
     this.load.image("wall","wallpaper.png");
     this.load.image("brick","brick.png");
@@ -33,7 +33,7 @@ function preload ()
 
 function create ()
 {
-    this.add.image(0, 0, "wall").setScale(10,10);
+    this.add.image(0, 0, "wall").setScale(5,5);
     mariop = this.physics.add.sprite(100, 10, "mario");
     mariop.setScale(2,2)
     plataforma = this.physics.add.staticGroup();
@@ -41,8 +41,32 @@ function create ()
     this.physics.add.collider(mariop, plataforma);
     mariop.setBounce(0.4);
     mariop.setCollideWorldBounds(true);
-    plataforma.create(600, 400, "brick").setScale(20,0.5).refreshBody();;
-
+    plataforma.create(600, 400, "brick").setScale(20,0.5).refreshBody();
+    this.anims.create({
+    key: "left",
+    frames: this.anims.generateFrameNumbers("mario", { start: 4, end: 3 }),
+    frameRate: 5,
+    repeat: -1
+});
+    this.anims.create({
+    key: "right",
+    frames: this.anims.generateFrameNumbers("mario", { start: 7, end: 8 }),
+    frameRate: 5,
+    repeat: -1
+});
+  this.anims.create({
+    key: "normal1",
+    frames: this.anims.generateFrameNumbers("mario", 5),
+    frameRate: 1,
+    repeat: -1
+});
+this.anims.create({
+    key: "normal2",
+    frames: this.anims.generateFrameNumbers("mario", 6),
+    frameRate: 1,
+    repeat: -1
+});
+    
 
 }
 
@@ -52,16 +76,20 @@ function update ()
     if (cursors.left.isDown)
     {
         mariop.setVelocityX(-160);
+        mariop.anims.play("left", true);
+        mariop.anims.play("normal1", true);
+
     
     }
-    else if (cursors.right.isDown)
+    if (cursors.right.isDown)
     {
         mariop.setVelocityX(160);
+        mariop.anims.play("right", true);
     
-    }
-    else
+    }else
     {
         mariop.setVelocityX(0);
+        mariop.anims.play("normal2", true);
     
     }
     
